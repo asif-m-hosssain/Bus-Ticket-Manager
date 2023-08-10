797b146bae3d45ff0c70e80e118eb6ed1d99c887
Pranta <kazialrefatpranta@gmail.com>
2023-08-10 19:55:41 +0600

    MVC pattern improved in publishing ticket

--- app/Http/Controllers/BrandTicketPublishedController.php
+++ app/Http/Controllers/BrandTicketPublishedController.php
@@ -8,55 +8,34 @@ use Illuminate\Support\Facades\DB;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Brand_Ticket_Published;
+use Carbon\Carbon;
 
 class BrandTicketPublishedController extends Controller
 {
     public function BrandAddTicketSubmit(Request $req)
+    
     {
-        // print_r($req->input());
-
         $author_id = Auth::user()->id;
-        $authod_name = Auth::user()->name;
-        $data = new Brand_Ticket_Published;
-        $data->b_comp_ticket_author_id = $author_id;
-        $data -> b_comp_ticket_from = $req->input("Start_RouteName");
-        $data -> b_comp_ticket_to = $req->input("Destination_RouteName");
-        $data -> b_comp_ticket_seat = $req->input("No_Seats");
-        $data -> b_comp_ticket_date = $req->input("Start_Time");
-        $data -> b_comp_ticket_price = $req->input("Ticket_Price");
-        $data -> b_comp_ticket_author_name = $authod_name;
-        
-        // seats
+        $author_name = Auth::user()->name;
 
+        $ticketData = [
+            'b_comp_ticket_author_id' => $author_id,
+            'b_comp_ticket_from' => $req->input('Start_RouteName'),
+            'b_comp_ticket_to' => $req->input('Destination_RouteName'),
+            'b_comp_ticket_seat' => $req->input('No_Seats'),
+            'b_comp_ticket_date' => $req->input('Start_Time'),
+            'b_comp_ticket_price' => $req->input('Ticket_Price'),
+            'b_comp_ticket_author_name' => $author_name,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
 
-        
+        $brandTicket = Brand_Ticket_Published::createTicket($ticketData);
 
-
-        $number_of_seats= $req->input("No_Seats");
-        $total_seats = array();
-        $empty_seats = array();
-        for ($i = 1; $i <= $number_of_seats; $i++) {
-            $total_seats[] = $i;
-            $empty_seats[$i] = False ;
-        }
-        // end of seats
-
-
-        // --
-        
-        $total_seats = serialize($total_seats);
-        $data -> all_seats = $total_seats;
-
-
-        $empty_seats = serialize($empty_seats);
-        $data -> empty_seats = $empty_seats;
-        // ==
-
-
-
-        $data -> save();
         return redirect()->back();
     }
+
+
     public function BrandDeleteTicketSubmit(request $req)
     {
         
--- app/Models/Brand_Ticket_Published.php
+++ app/Models/Brand_Ticket_Published.php
@@ -7,6 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand_Ticket_Published extends Model
 {
-    protected $table="bus_company_published_ticket";
-    public $timestamp = false;
+    use HasFactory;
+
+    protected $table = "bus_company_published_ticket";
+    public $timestamps = false;
+
+    protected $fillable = [
+        'b_comp_ticket_author_id', // Add other fillable fields here
+        'b_comp_ticket_from',
+        'b_comp_ticket_to',
+        'b_comp_ticket_seat',
+        'b_comp_ticket_date',
+        'b_comp_ticket_price',
+        'b_comp_ticket_author_name',
+        'all_seats',
+        'empty_seats',
+        'created_at', 
+        'updated_at'
+    ];
+    
+
+    public static function createTicket(array $ticketData)
+    {
+        $ticketData['all_seats'] = self::serializeSeats(range(1, $ticketData['b_comp_ticket_seat']));
+        $ticketData['empty_seats'] = self::serializeSeats(array_fill(1, $ticketData['b_comp_ticket_seat'], false));
+
+        return self::create($ticketData);
+    }
+
+    private static function serializeSeats($seats)
+    {
+        return serialize($seats);
+    }
 }
